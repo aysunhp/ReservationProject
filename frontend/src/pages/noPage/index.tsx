42a2ded4e679f56c2b310a6361e6eb18bf89dfe8
aysunhp <bd7zatzha@code.edu.az>
2024-02-07 22:09:11 +0400

    no page added

--- frontend/src/pages/noPage/index.tsx
+++ frontend/src/pages/noPage/index.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import "./noPage.scss";
+import { Button} from 'antd';
 
-type Props = {}
 
-const NoPage = (props: Props) => {
+const NoPage = () => {
   return (
-    <div>NoPage</div>
+ <div className="no-page">
+  <div className="no-page-sect">
+
+<div className="container">
+<div className="no-page-item">
+  <img src="https://modmixmap.travelerwp.com/wp-content/themes/traveler/v3/images/404.png" alt="" />
+</div>
+<h1>Oops! Look like you're lost</h1>
+<p>Either something went wrong or the page doesn't exist anymore.</p>
+<Link to="/">
+<Button type="primary"  size="large" className="go-home-btn">
+          Go to Home
+        </Button>
+</Link>
+</div>
+</div>
+ </div>
   )
 }
 
